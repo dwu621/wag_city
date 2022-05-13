193313bedbe4f6da459a3717319aa479b6d67eaa
David Wu <dwu0508@gmail.com>
2022-05-12 22:10:15 -0500

    added DeleteUser

--- client/src/services/UserServices.js
+++ client/src/services/UserServices.js
@@ -17,3 +17,13 @@ export const GetUserInfo = async (id) => {
         throw error
     }
 }
+
+export const DeleteUser = async (id) => {
+    try {
+        const res = await Client.delete(`api/user/${id}`)
+        console.log(res)
+        return res
+    } catch (error) {
+        throw error
+    }
+}
