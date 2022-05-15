2cbc863a67423dd017f683db591cb6f55ed9299c
David Wu <dwu0508@gmail.com>
2022-05-15 09:47:59 -0500

    added stub dog

--- client/src/pages/Dog.js
+++ client/src/pages/Dog.js
@@ -1,6 +1,28 @@
+import { DataContext } from "../components/DataContext";
+import { useContext, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+
 const Dog = () => {
-    return (
-        <div>this is dog</div>
+    const { user, authenticated } = useContext( DataContext )
+    const { ownerInfo, setOwnerInfo } = useState(null)
+    
+    const navigate = useNavigate()
+    
+    const getOwnerInfo = async (id) => {
+        console.log(`getting info for ${id}`)
+    }
+
+    useEffect(() => {
+        if(user) {
+            getOwnerInfo(user.id)
+        }
+    }, [user])
+
+    return (user && user.userType === "Walker") ? (
+        <div>{navigate("../jobs")}</div>
+    ) : (
+        <div>owener</div>
     )
 }
 
