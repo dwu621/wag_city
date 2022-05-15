330878fc9bebd5d649f5044d0cb630b0911805d9
David Wu <dwu0508@gmail.com>
2022-05-15 10:48:46 -0500

    card displaying

--- client/src/pages/Dog.js
+++ client/src/pages/Dog.js
@@ -2,17 +2,22 @@ import { DataContext } from "../components/DataContext";
 import { useContext, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { GetOwnerDogs } from "../services/UserServices";
+import Container from "react-bootstrap/esm/Container";
+import Card from "react-bootstrap/Card"
+import CardHeader from "react-bootstrap/esm/CardHeader";
+import Row from "react-bootstrap/esm/Row";
+import Button from "react-bootstrap/esm/Button";
 
 
 const Dog = () => {
     const { user, authenticated } = useContext( DataContext )
-    const { ownerDogs, setOwnerDogs } = useState(null)
+    const [ownerDogs, setOwnerDogs] = useState([])
     
     const navigate = useNavigate()
     
     const handleOwnerInfo = async (id) => {
         const dogs = await GetOwnerDogs(id)
-        console.log(dogs)
+        setOwnerDogs(dogs)
     }
 
     useEffect(() => {
@@ -23,8 +28,24 @@ const Dog = () => {
 
     return (user && user.userType === "Walker") ? (
         <div>{navigate("../jobs")}</div>
-    ) : (
-        <div>owener</div>
+    ) : (user && authenticated && ownerDogs) && (
+        <Container>
+          <Row>
+          <h2>{user.firstName}'s Dogs</h2>
+          {
+              ownerDogs.map((dog)=>(
+                <Card key={dog.id} style={{ width: '18rem' }}>
+                    <Card.Img src={dog.image} />
+                </Card>
+              ))
+          }
+
+          </Row>  
+          <Button>
+              Add New Dog
+          </Button>
+        </Container>
+        
     )
 }
 
