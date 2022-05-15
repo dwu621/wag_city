31553810a9fa86a4a9e1acaeb9cfbbe77efc9a72
David Wu <dwu0508@gmail.com>
2022-05-15 17:19:53 -0500

    made indentations to read easier

--- client/src/pages/Dog.js
+++ client/src/pages/Dog.js
@@ -86,35 +86,63 @@ const Dog = () => {
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="name">
-                        <Form.Control type="text" name="name" placeholder="Name" onChange={handleChange} />
+                        <Form.Control 
+                        type="text" name="name" 
+                        placeholder="Name" 
+                        onChange={handleChange} />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="breed">
-                        <Form.Control type="text" name="breed" placeholder="Breed" onChange={handleChange} />
+                        <Form.Control 
+                        type="text" 
+                        name="breed" 
+                        placeholder="Breed" 
+                        onChange={handleChange} />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="gender">
-                        <Form.Control type="text" name="gender" placeholder="Gender" onChange={handleChange} />
+                        <Form.Control 
+                        type="text" 
+                        name="gender" 
+                        placeholder="Gender" 
+                        onChange={handleChange} />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="weight">
-                        <Form.Control type="text" name="weight" placeholder="Weight(lbs)" onChange={handleChange} />
+                        <Form.Control 
+                        type="text" 
+                        name="weight" 
+                        placeholder="Weight(lbs)" 
+                        onChange={handleChange} />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="image">
-                        <Form.Control type="text" name="image" placeholder="Picture" onChange={handleChange} />
+                        <Form.Control 
+                        type="text" 
+                        name="image" 
+                        placeholder="Picture" 
+                        onChange={handleChange} />
                     </Form.Group>
 
-                    <Button disabled={
+                    <Button 
+                    disabled={
                         !formValues.name || 
                         !formValues.gender || 
                         !formValues.breed ||
                         !formValues.weight ||
                         !formValues.image ||
-                        !formValues.ownerId} type="submit" onClick={() => handleClick()} >Add Dog</Button>
+                        !formValues.ownerId} 
+                    type="submit" 
+                    onClick={() => handleClick()}>
+                            Add Dog
+                    </Button>
                      <>     </>   
                     
-                    <Button onClick={() => handleClick()} variant="outline-primary">Cancel</Button>
+                    <Button 
+                    onClick={() => handleClick()} 
+                    variant="outline-primary">
+                        Cancel
+                    </Button>
                 </Form>                
        
             </Container>
